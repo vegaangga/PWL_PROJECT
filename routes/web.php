ce4af1d7ea4aeb0fe74b066baa93f3ac9f3efc01
vegaangga <vegaangga27@gmail.com>
2021-05-27 00:11:16 +0700

    menaruh route pada group middleware

--- routes/web.php
+++ routes/web.php
@@ -22,24 +22,46 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 // Route::get('/', [App\Http\Controllers\HomeController::class, 'welcome'])->name('welcome');
 
-Route::prefix('siswa')->group(function () {
-    Route::get('/', [SiswaController::class,'index'])->name('siswa.index');
-    Route::get('/profile', [SiswaController::class,'profile'])->name('siswa.profile');
-    Route::get('/formulir', [SiswaController::class,'formulir'])->name('siswa.formulir');
-    Route::get('/cetak/{siswa}', [SiswaController::class,'cetak'])->name('siswa.cetak');
-});
+// Route::prefix('siswa')->group(function () {
+//     Route::get('/', [SiswaController::class,'index'])->name('siswa.index');
+//     Route::get('/profile', [SiswaController::class,'profile'])->name('siswa.profile');
+//     Route::get('/formulir', [SiswaController::class,'formulir'])->name('siswa.formulir');
+//     Route::get('/cetak/{siswa}', [SiswaController::class,'cetak'])->name('siswa.cetak');
+// });
 
 Route::resource('siswa', SiswaController::class);
 
 //Route::get('mahasiswa/cetak_pdf/{nim}', [MahasiswaController::class, 'cetak_pdf'])->name('mahasiswa.cetak_pdf');
 
-Route::prefix('admin')->group(function () {
+// Route::prefix('admin')->group(function () {
+//     Route::get('/data-siswa', [HomeController::class,'datasiswa'])->name('admin.datasiswa');
+//     Route::get('/data-ortu', [HomeController::class,'dataortu'])->name('admin.dataortu');
+//     Route::get('/biaya-daftar', [HomeController::class,'biayadaftar'])->name('admin.biayadaftar');
+//     Route::get('/daftar-ulang', [HomeController::class,'daftarulang'])->name('admin.daftarulang');
+//     Route::get('/cetak', [HomeController::class,'cetak'])->name('cetak');
+// });
+
+
+Route::middleware(['auth', 'ceklevel:0,1'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/', [SiswaController::class,'index'])->name('siswa.index');
+    Route::get('/profile', [SiswaController::class,'profile'])->name('siswa.profile');
+    Route::get('/formulir', [SiswaController::class,'formulir'])->name('siswa.formulir');
+    Route::get('/cetak/{siswa}', [SiswaController::class,'cetak'])->name('siswa.cetak');
+
     Route::get('/data-siswa', [HomeController::class,'datasiswa'])->name('admin.datasiswa');
     Route::get('/data-ortu', [HomeController::class,'dataortu'])->name('admin.dataortu');
     Route::get('/biaya-daftar', [HomeController::class,'biayadaftar'])->name('admin.biayadaftar');
     Route::get('/daftar-ulang', [HomeController::class,'daftarulang'])->name('admin.daftarulang');
     Route::get('/cetak', [HomeController::class,'cetak'])->name('cetak');
 });
+
+// Route::prefix('siswa')->group(function () {
+//     Route::get('/', [SiswaController::class,'index'])->name('siswa.index');
+//     Route::get('/profile', [SiswaController::class,'profile'])->name('siswa.profile');
+//     Route::get('/formulir', [SiswaController::class,'formulir'])->name('siswa.formulir');
+//     Route::get('/cetak/{siswa}', [SiswaController::class,'cetak'])->name('siswa.cetak');
+// });
