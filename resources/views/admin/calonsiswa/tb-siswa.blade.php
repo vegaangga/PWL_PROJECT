c676e71f0434f105ff17b81bf0b996c5815e1797
= <=>
2021-05-31 21:37:55 +0700

    Create view tb-siswa

---
+++ resources/views/admin/calonsiswa/tb-siswa.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.main')
+@section('menu_home', 'active')
+@section('content')
+<div class="row">
+   <div class="col-lg-12 mb-4">
+       <!-- Approach -->
+
+        <div class="card shadow mb-4">
+
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Data Biaya Pendaftaran</h6>
+            </div>
+            <div class="card-body">
+                <div class="search-icon-box card-box mb-30">
+                    <input type="text" class="form-control" id="myInput" placeholder="Search" title="Type in a name"
+                        style="background-color:rgb(250, 252, 252)">
+                    <i class="search_icon dw dw-search"></i>
+                </div>
+                <table class="data-table table nowrap">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>User_id</th>
+                            <th>NISN</th>
+                            <th>Nama</th>
+                            <th>email</th>
+                            <th>No_telp</th>
+                            <th>Foto</th>
+                            <th>Tgl_lahir</th>
+                            <th>Tempat_lahir</th>
+                            <th>Alamat</th>
+                            <th>Asal_sekolah</th>
+                            <th>Status_ayah</th>
+                            <th>Nama_ayah</th>
+                            <th>Nik_ayah</th>
+                            <th>Pekerjaan_ayah</th>
+                            <th>Gaji_ayah</th>
+                            <th>Status_ibu</th>
+                            <th>Nama_ibu</th>
+                            <th>Nik_ibu</th>
+                            <th>Pekerjaan_ibu</th>
+                            <th>Gaji_ibu</th>
+                        </tr>
+                    </thead>
+                    <tbody id="myTable">
+                        @foreach($datas as $data)
+                        <tr>
+                            <td>{{ $data->user->nisn}}</td>
+                            <td>{{ $data->user->name }}</td>
+                            <td>Lihat Struk</td>
+                            <td>
+                                @if ($data->status == 'belum')
+                                    <label class="badge badge-warning">Belum Terverifikasi</label>
+                                @else
+                                    <label class="badge badge-success">Sudah Terverifikasi</label>
+                                @endif
+                            </td>
+                            <td>
+                                @if ($data->status == 'belum')
+                                <form action="{{ route('daftar.update', $data->id) }}" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('PUT')
+                                    <button class="btn btn-info btn-sm" onclick="return confirm('Anda yakin struk sudah benar?')">Verifikasi
+                                    </button>
+                                </form>
+                                @else
+                                <form action="{{ route('daftar.destroy', $data->id) }}" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="btn btn-info btn-sm" onclick="return confirm('Hapus Data?')">Hapus
+                                    </button>
+                                </form>
+                                @endif
+                            </td>
+                        </tr>
+                        @endforeach
+
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+
+
+
+    </div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        $("#myInput").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#myTable tr").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+    });
+
+</script>
+
+
+@endsection
