239d919848356fafcfe5392c2c2f4b9fb7ba4d3e
= <=>
2021-06-07 16:07:37 +0700

    Edit Data User Terdaftar

--- resources/views/admin/data/tb-dataterdaftar.blade.php
+++ resources/views/admin/data/tb-dataterdaftar.blade.php
@@ -8,7 +8,7 @@
         <div class="card shadow mb-4">
 
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Data Daftar Ulang</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Data User Terdaftar</h6>
             </div>
             <div class="card-body">
                 <div class="search-icon-box card-box mb-30">
