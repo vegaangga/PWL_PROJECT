31cf371a6f2a8c8ad280c49c8fc4e4180e327944
vegaangga <vegaangga27@gmail.com>
2021-06-08 13:44:27 +0700

    Mengedit User Controller Tambah dan Hapus

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Biaya;
 use App\Models\User;
+use BiayaDaftar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class UserController extends Controller
@@ -41,7 +45,11 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        if(Auth::user()->level == '1') {
+            Alert::info('Oopss..', 'Anda dilarang masuk ke area ini.');
+            return redirect()->to('/login');
+        }
+        return view('auth.create');
     }
 
     /**
@@ -52,7 +60,27 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'nisn' => 'required|string|max:20|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->input('name'),
+            'nisn' => $request->input('nisn'),
+            'email' => $request->input('email'),
+            'password' => bcrypt(($request->input('password'))),
+        ]);
+
+        Session::flash('message', 'Berhasil ditambahkan!');
+        Session::flash('message_type', 'success');
+
+        alert()->success('Berhasil.','Data telah ditambahkan!');
+        return redirect()->route('user.index');
+
     }
 
     /**
@@ -97,6 +125,20 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        if(Auth::user()->id != $id) {
+            $user_data = User::findOrFail($id);
+            $user_biaya = Biaya::findOrFail($id);
+            $user_biaya->delete();
+            $user_data->delete();
+            Session::flash('message', 'Berhasil dihapus!');
+            Session::flash('message_type', 'success');
+        } else {
+            Session::flash('message', 'Akun anda sendiri tidak bisa dihapus!');
+            Session::flash('message_type', 'danger');
+        }
+
+
+        alert()->success('Berhasil.','Data telah dihapus!');
+        return redirect()->route('user.index');
     }
 }
