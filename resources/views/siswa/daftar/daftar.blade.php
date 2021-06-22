a789666b84cf37b2a8470df9f07838ae64edceae
vegaangga <vegaangga27@gmail.com>
2021-06-22 11:17:34 +0700

    memperbaiki route daftar siswa

--- resources/views/siswa/daftar/daftar.blade.php
+++ resources/views/siswa/daftar/daftar.blade.php
@@ -8,8 +8,10 @@
     $c = Auth::user()->verif_dau;
     @endphp
     @include('layouts.petunjuk')
+
    <div class="col-lg-12 mb-4">
        <!-- Approach -->
+       @if($a != null)
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Struk Daftar</h6>
@@ -20,13 +22,13 @@
                 <p>
                     Biaya Pendaftaran Untuk PPDB SMAN 1 Puri adalah 150.000
                 </p>
-                <a href="{{ route('daftar.create') }}" type="button" class="btn" data-bgcolor="#3b5998" data-color="#ffffff">
+                <a href="{{ route('biaya.create') }}" type="button" class="btn" data-bgcolor="#3b5998" data-color="#ffffff">
                     <i class="icon-copy fa fa-user-plus" aria-hidden="true"></i>
                     Upload Struk Pembayaran
                 </a>
                 @endif
 
-                @if ($a=='true')
+                @if ($a=='0')
                 <p>Tunggu Admin Mengkonfirmasi Struk Anda </p>
                 @endif
                 <div class="table-responsive">
@@ -65,11 +67,17 @@
                 </div>
             </div>
         </div>
+        @endif
+
+        @if($b != null)
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Data Siswa</h6>
             </div>
             <div class="card-body">
+                @if ($a=='0')
+                <p>Tunggu Admin Mengkonfirmasi Data Anda </p>
+                @endif
                 <table class="table table-bordered" id="dataTable">
                     <thead>
                         <tr>
@@ -97,7 +105,63 @@
 
             </div>
         </div>
-    </div>
+        @endif
+        @if($c != null)
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Struk Daftar</h6>
+            </div>
+            <div class="card-body">
 
+                @if($c == null)
+                <p>
+                    Biaya Daftar Ulang Untuk PPDB SMAN 1 Puri adalah Rp. 1.000.000
+                </p>
+                <a href="{{ route('biaya.create') }}" type="button" class="btn" data-bgcolor="#3b5998" data-color="#ffffff">
+                    <i class="icon-copy fa fa-user-plus" aria-hidden="true"></i>
+                    Upload Struk Pembayaran
+                </a>
+                @endif
+                @if ($a=='0')
+                <p>Tunggu Admin Mengkonfirmasi Struk Anda </p>
+                @endif
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable">
+                        <thead>
+                            <tr>
+                                <th class="sorting sorting_asc">User ID</th>
+                                <th>Nama</th>
+                                <th>Struk</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+
+                        <tbody id="myTable">
+
+                            @foreach($biaya as $data)
+                            <tr>
+                                <td>{{ $data->user_id}}</td>
+                                <td>{{ $data->user->name }}</td>
+                                <td>
+                                    <a href="{{route('biaya.show', $data->user_id)}}">
+                                        Lihat Struk
+                                    </a>
+                                </td>
+                                <td>
+                                    @if ($data->status == 'belum')
+                                        <label class="badge badge-warning">Belum Terverifikasi</label>
+                                    @else
+                                        <label class="badge badge-success">Sudah Terverifikasi</label>
+                                    @endif
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        @endif
+    </div>
 </div>
 @endsection
